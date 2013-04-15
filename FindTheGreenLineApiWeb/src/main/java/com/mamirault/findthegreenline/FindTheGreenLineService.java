87adcbd9a23d26c60890464d95d9d0ca2e085ccd
Michael Amirault <mamirault@hubspot.com>
2013-04-15 11:38:15 -0400

    CORS

--- FindTheGreenLineApiWeb/src/main/java/com/mamirault/findthegreenline/FindTheGreenLineService.java
+++ FindTheGreenLineApiWeb/src/main/java/com/mamirault/findthegreenline/FindTheGreenLineService.java
@@ -28,10 +28,10 @@ public class FindTheGreenLineService extends Service<FindTheGreenLineConfigurati
     final String defaultName = configuration.getDefaultName();
     
     FilterBuilder filterBuilder = environment.addFilter(CrossOriginFilter.class, "/*");
-    filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://localhost:3333");
-    filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://ec2-50-19-184-2.compute-1.amazonaws.com:3333");
-    filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "ec2-50-19-184-2.compute-1.amazonaws.com:3333");
-    filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://findthegreenline.com");
+    filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");//http://localhost:3333");
+    //filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://ec2-50-19-184-2.compute-1.amazonaws.com:3333");
+    //filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "ec2-50-19-184-2.compute-1.amazonaws.com:3333");
+    //filterBuilder.setInitParam(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://findthegreenline.com");
 
     environment.addResource(new StopResource(template, defaultName));
     environment.addResource(new StationResource(template, defaultName));
--- FindTheGreenLineApp/app/collections/stops.coffee
+++ FindTheGreenLineApp/app/collections/stops.coffee
@@ -3,7 +3,8 @@ Collection = require './collection'
 Stop       = require '../models/stop'
 
 class Stops extends Collection
-  urlBase : "http://api.findthegreenline.com/stops/all"
+  #urlBase : "http://api.findthegreenline.com/stops/all"
+  urlBase : "http://localhost:8080/stops/all"
   model   : Stop
 
   fetch: (callback, args) =>
